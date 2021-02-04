d99b1f6b58f73b1a1d65aa4ff193ce4b801b686a
ll-lingling <782540706@qq.com>
2021-02-04 14:06:55 +0800

    .

--- Java-Module9/lg-visualization/src/main/java/com/lagou/service/IAccountService.java
+++
@@ -1,7 +0,0 @@
-package com.lagou.service;
-
-public interface IAccountService {
-
-    //转账
-    public int transfer(String inName, String outName , double money);
-}
---
+++ Java-Module9/可视化作业/lg-visualization/src/main/java/com/lagou/mapper/IdustryMapper.java
@@ -0,0 +1,31 @@
+package com.lagou.mapper;
+
+import com.lagou.vo.ConverterVo;
+import com.lagou.vo.IndustryVo;
+import com.lagou.vo.SalaryVo;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface IdustryMapper {
+    /**
+     * 统计各个城市招聘情况
+     * @return
+     */
+    public List<ConverterVo> countCityPosition() ;
+
+    List<IndustryVo> countIndustryTop();
+
+    /**
+     * 统计热门行业对比
+     * @param industryName
+     * @param startDate
+     * @param endDate
+     * @return
+     */
+    List<IndustryVo> getIndustryData(@Param("industryName") String industryName,
+                                     @Param("startDate") String startDate,
+                                     @Param("endDate") String endDate);
+
+    List<SalaryVo> countSalaryTop();
+}
